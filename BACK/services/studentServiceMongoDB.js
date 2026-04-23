4c7c5657b6c9a954caec5186307026f01e57dee8
ivan.bomko <vanya.bomko@gmail.com>
2026-04-23 02:28:45 +0200

    feat: studentServiceMongoDb.js funcs added

---
+++ BACK/services/studentServiceMongoDB.js
@@ -0,0 +1,32 @@
+import userModel from "../models/userModel";
+import User from "../models/userModel";
+import bcrypt from "bcrypt";
+
+
+export const findAllStudents = () => {
+    return await User.find({});
+};
+
+
+export const findAllStudentsById = (id) => {
+    return await User.findById(id);
+};
+
+
+export const createStudentService = async (data) => {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    return await User.create({
+        ...data,
+        password: hashedPassword,
+    });
+};
+
+
+export const udpateStudentService = async (id, data) => {
+    return await User.findByIdAndUpdate(id, data, { new: true });
+};
+
+
+export const deleteStudentService = (id) => {
+    return await User.findByIdAndDelete(id);
+};  
