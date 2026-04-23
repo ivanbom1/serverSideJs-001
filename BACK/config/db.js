3d04ecad4596caedc0c96558894e105f09ee3033
ivan.bomko <vanya.bomko@gmail.com>
2026-04-23 02:14:52 +0200

    feat: db.js with mongodb connect func

---
+++ BACK/config/db.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import mongoose from "mongoose";
+
+
+export const connectMongoDB = async () => {
+    try {
+	await mongoose.connect(process.env.MONGO_URI);
+	console.log("Connected to MongoDB");
+} catch (err) {
+	console.error(err);
+	process.exit(1); 
+}
+};
