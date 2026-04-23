97233d05807c91a0c70074ec171d45e10ee41f4c
ivan.bomko <vanya.bomko@gmail.com>
2026-04-23 02:15:39 +0200

    deps: mongoose installation and model creation

---
+++ BACK/models/userModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+  name:     { type: String, required: true },
+  email:    { type: String, required: true, unique: true },
+  gpa:      { type: Number, required: true },
+  password: { type: String, required: true },
+}
+);
+
+export default mongoose.model("User", userSchema);
