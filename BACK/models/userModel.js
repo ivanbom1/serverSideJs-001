cd93fea89af7c7a05e4128e5ac07db204412773e
ivan.bomko <vanya.bomko@gmail.com>
2026-05-02 20:51:10 +0200

    feat(validation): student validation method added, userModel params modified

--- BACK/models/userModel.js
+++ BACK/models/userModel.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema(
   email:    { type: String, required: true, unique: true },
   gpa:      { type: Number, required: true },
   password: { type: String, required: true },
+  major: {type: String, required: true},
 }
 );
 
