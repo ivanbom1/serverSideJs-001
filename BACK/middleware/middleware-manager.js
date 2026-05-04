56b4e9592934585eb1d440ecd0795def476bbb8e
ivan.bomko <vanya.bomko@gmail.com>
2026-05-04 02:07:18 +0200

    event(middleware): import added to middleware manager

--- BACK/middleware/middleware-manager.js
+++ BACK/middleware/middleware-manager.js
@@ -1,3 +1,4 @@
 export { validateStudent } from "./valideStudent.js"
 export { authCheck } from "./auth-middleware.js"
 export { default as multerConfig } from "./multer-config.js"
+export { isPresident } from "./isPresident.js"
